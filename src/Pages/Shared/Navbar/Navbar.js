ee3f6bc80b42b6e1dd989ca1507c45b301089864
masumwbdev <masumwbd@gmail.com>
2022-04-28 04:29:55 +0600

    added banner and linked with pages

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -8,23 +8,37 @@ const Navbar = () => {
         <div>
             <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">MI SHOP</a>
+                    <Link className='text-light' to="/home"><span>MI SHOP</span></Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="main_nav">
                         <ul class="navbar-nav">
                             <li class="nav-item active"> <Link to="/home"><a class="nav-link" href="#"> Home </a></Link> </li>
-                            <li class="nav-item"><Link to="/login"><a class="nav-link" href="#"> About </a></Link></li>
-                            <li class="nav-item"><a class="nav-link" href="#"> Services </a></li>
+                            <li class="nav-item"><Link to="/smartphones"><a class="nav-link" href="#"> Smartphones </a></Link></li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">  Category  </a>
+                                <Link to="/phones-accessories"><a class="nav-link" href="#"> Phones Accessories </a></Link>
                                 <ul class="dropdown-menu fade-up">
-                                    <li><a class="dropdown-item" href="#"> Submenu item 1</a></li>
-                                    <li><a class="dropdown-item" href="#"> Submenu item 2 </a></li>
-                                    <li><a class="dropdown-item" href="#"> Submenu item 3 </a></li>
+                                    <li><Link to="/chargers">Charger</Link></li>
+                                    <li><Link to="/ear-phone">Ear-phone</Link></li>
+                                    <li><Link to="/phone-cover">Phone Cover</Link></li>
+                                    <li><Link to="/usb-cable">USB Cable</Link></li>
                                 </ul>
                             </li>
+                            <li class="nav-item"><Link to="/laptops"><a class="nav-link" href="#"> Laptops </a></Link></li>
+                            <li class="nav-item dropdown">
+                                <Link to="/laptops-accessories"><a class="nav-link" href="#"> Laptops Accessories </a></Link>
+                                <ul class="dropdown-menu fade-up">
+                                    <li><Link to="/headphones">Head Phone</Link></li>
+                                    <li><Link to="/keyboards">Keyboard</Link></li>
+                                    <li><Link to="/mouse">Mouse</Link></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item"><Link to="/tablets"><a class="nav-link" href="#"> Tablets </a></Link></li>
+                            <li class="nav-item"><Link to="/tv"><a class="nav-link" href="#"> Tv </a></Link></li>
+                            <li class="nav-item"><Link to="/mi-routers"><a class="nav-link" href="#"> Routers </a></Link></li>
+                            <li class="nav-item"><Link to="/camera"><a class="nav-link" href="#"> Camera </a></Link></li>
+
                         </ul>
                         <ul class="navbar-nav ms-auto">
                             <button>Login</button>
